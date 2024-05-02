65cf35161965cdb2fe816aba077efde3c910b9d3
kieransweetman <kikisweetman@gmail.com>
2024-05-02 14:13:04 +0200

    Static primitive methods tp4

---
+++ utils/StaticMethodTest.java
@@ -0,0 +1,17 @@
+package utils;
+
+public class StaticMethodTest {
+    public static void main(String[] args) {
+        String str = "12";
+
+        int a = 1;
+        int b = 2;
+
+        System.out.println(Integer.parseInt(str));
+
+        int max = Integer.max(a, b);
+        System.out.println(max);
+
+    }
+
+}
