5223225ec64389fdcef7826ffc58826e20a27caa
kieransweetman <kikisweetman@gmail.com>
2024-05-02 14:05:22 +0200

    setters, getters, and common methods

--- entities/TestPerson.java
+++ entities/TestPerson.java
@@ -7,14 +7,24 @@ public class TestPerson {
         PostalAddress add1 = new PostalAddress(1, "Main St", 12345, "Springfield");
         PostalAddress add2 = new PostalAddress(2, "Broadway", 54321, "New York");
 
-        Person p1 = new Person("John", "Doe", add1);
-        Person p2 = new Person("Jane", "Doe", add2);
+        Person p1 = new Person("John", "Doe");
+        Person p2 = new Person("Jane", "Doe");
 
         System.out.println("p1: " + p1 + "\np2: " + p2);
 
         // we need to make sure the visibilty is public for name if we want to acces it
         // from another class instnace
-        System.out.println("this wont work!" + p1.name);
+        System.out.println("name is public: " + p1.name);
+
+        p1.setAddress(add1);
+        p2.setAddress(add2);
+
+        p1.printFullName();
+        p1.setName("Kieran");
+        p1.setLastName("Sweetman");
+        p1.printFullName();
+
+        System.out.println("p1: " + p1 + "\np2: " + p2);
     }
 
 }
--- entities2/Person.java
+++ entities2/Person.java
@@ -8,10 +8,48 @@ public class Person {
     String lastName;
     PostalAddress address;
 
+    // constructors
+    public Person(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
     public Person(String name, String lastName, PostalAddress address) {
         this.name = name;
         this.lastName = lastName;
         this.address = address;
+    }
+
+    // getters and setters
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public PostalAddress getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(PostalAddress address) {
+        this.address = address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    // methods
+
+    public void printFullName() {
+        System.out.println("\n" + this.lastName.toUpperCase() + " " + this.name);
     }
 
     @Override
